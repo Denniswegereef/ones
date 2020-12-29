ce79ddb5d43e33ab7f213fb2f6fd28a21ea27975
Denniswegereef <denniswegereef@gmail.com>
2020-12-29 20:00:22 +0100

    fixed build

--- src/pages/model/[index].js
+++ src/pages/model/[index].js
@@ -1,32 +1,7 @@
-// import { fetchEntries } from '../../utils/ContentFulData'
+const Model = () => {
+  return (
+    <h1>Model</h1>
+  );
+}
 
-// const Serie = ({ data }) => {
-//   console.log(data)
-//   return (
-//     <>
-//       <h1>Series</h1>
-//     </>
-//   );
-// }
-
-// export default Serie;
-
-// export async function getStaticProps({ params }) {
-//   const res = await fetchEntries({
-//     content_type: 'serie',
-//   })
-
-//   return { props: { data: res },
-//   }
-// }
-
-// export async function getStaticPaths() {
-//   const res = await fetchEntries({
-//     content_type: 'serie'
-//   })
-
-//   return {
-//     paths: res.map(({fields}) => `/serie/${fields.slug}`),
-//     fallback: false,
-//   }
-// }
+export default Model;
---
+++ src/pages/serie/[index].js
@@ -0,0 +1,7 @@
+const Serie = () => {
+  return (
+    <h1>Serie</h1>
+  );
+}
+
+export default Serie;
